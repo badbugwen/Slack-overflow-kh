f4e8eee4f13e478d9ea4216ce81a55069aa1e1fb
Kafka Tzeng <freefish0308@gmail.com>
2018-07-15 12:33:02 +0800

    fix conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715030437) do
+ActiveRecord::Schema.define(version: 20180715034947) do
 
   create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "solutions", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
