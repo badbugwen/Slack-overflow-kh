25e29b81049228156ca1023ed0d4c2ef720fca36
Allie <zxp800310@gmail.com>
2018-07-19 06:36:46 +0800

    add user_id to model question & show question count on user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715040945) do
+ActiveRecord::Schema.define(version: 20180718222452) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180715040945) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "solutions", force: :cascade do |t|
