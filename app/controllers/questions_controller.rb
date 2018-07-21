0a70e66cd58f7b04a5bb737a83bebfb32483f5d4
Student X <sweewen45@gmail.com>
2018-07-21 12:53:10 +0800

    odered solutions by upvotes number

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,6 +21,7 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find_by(id: params[:id])
+    @solutions =@question.solutions.order(upvotes_count: :desc)
   end
 
   def destroy
