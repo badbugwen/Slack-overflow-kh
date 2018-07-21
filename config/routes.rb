8255fb5625ba2070b9306b22c97743a8a43caa3a
Student X <sweewen45@gmail.com>
2018-07-21 10:06:30 +0800

    add back question#new and create in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update]
-  resources :questions, only: [:index, :show] do
+  resources :questions, only: [:index, :show, :new, :create] do
     resources :solutions, only: [:create]
   end
     
