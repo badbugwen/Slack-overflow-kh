fcca25dead619702fc0c6b556d01421219f76b23
beranemo <beranemo@gmail.com>
2018-07-22 11:05:52 +0800

    add the relaiton function of favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show, :favorite]
   
   def index
   end
   
   def edit
-    @user = User.find(params[:id])
     unless @user == current_user
       flash[:alert] = "Not allow"
       redirect_back(fallback_location: root_path)  
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user)
     else
@@ -20,11 +19,10 @@ class UsersController < ApplicationController
     end    
   end
   
-  def show
+  private
+  def set_user
     @user = User.find(params[:id])
   end
-  
-  private
   
   def user_params
     params.require(:user).permit(
