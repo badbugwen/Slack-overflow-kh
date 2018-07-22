6cd73b5c852bd6fb7875f2542a9e18244db4cfd0
Allie <zxp800310@gmail.com>
2018-07-22 14:16:46 +0800

    setup relation among user, question, and upvote

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,7 +11,8 @@
 #
 class Question < ApplicationRecord
   has_many :solutions
-  has_many :upvotes
+  has_many :upvotes, dependent: :destroy
+  has_many :upvoted_users, through: :upvotes, source: :user
   has_many :favorites
-  belongs_to :user 
+  belongs_to :user
 end
