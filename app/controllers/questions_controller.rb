fdf081cb6d2f110fbb9c25b284e3f7284dc33d11
Student X <sweewen45@gmail.com>
2018-07-24 08:28:31 +0800

    order question#index and user#favorites by created_at and delete user#index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @questions = Question.all.page(params[:page]).per(10)
+    @questions = Question.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def new
