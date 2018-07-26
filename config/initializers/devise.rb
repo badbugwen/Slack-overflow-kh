d76ff865f433741ffe403f0ae34e5958afef1d81
Student X <sweewen45@gmail.com>
2018-07-26 14:31:33 +0800

    Add Omniauth Your Initializers. access app_id and secret by github.yml and gitignore it

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,13 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  github_config = Rails.application.config_for(:github)
+  #omniauth
+  config.omniauth :github, github_config["app_id"], github_config["secret"],
+  cope: "user:email,user:follow", info_fields: "email, name", 
+  callback_url:"http://localhost:3000/users/auth/github/callback"
+  end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
